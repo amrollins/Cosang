e5166a0a0578f610c10a4e5f8001adb77490b030
amrollins <102754385+amrollins@users.noreply.github.com>
2023-01-11 15:04:14 -0600

    idea for multiple files implemented

--- alexis_version_clean_analysis.py
+++ alexis_version_clean_analysis.py
@@ -25,43 +25,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math 
 
-gal_coord = [27.5708,29.1913,27.5166] ##hardcoded, make into argument
-f = h5py.File('test_tag.h5', 'r') #opens the file
-f_key = list(f.keys())
-print(f_key) ##shows what keys we have to work with
+gal_coord = [27.5708,29.1913,27.5166] ##hardcoded, make into argument, maybe figure out a process to get this
+list_met = []
+list_x = []
+list_y = []
+list_z = []
+list_stell_m = []
 
-##gets the data and puts it in the right format
-def make_for_calc(ds, neginf = False, center_val = []):
-    if neginf == True:
-        new = np.nan_to_num(np.array(ds), neginf = 0.0)
-    else:
-        new = np.nan_to_num(np.array(ds))
+
+for j in args.path: #FIXME, make arguments for path to snapshot files
+    f = h5py.File(str(j), 'r') #opens the file
+    f_key = list(f.keys())
+    print(f_key) ##shows what keys we have to work with
+    
+    ##gets the data and puts it in the right format
+    def make_for_calc(ds, neginf = False, center_val = []):
+        if neginf == True:
+            new = np.nan_to_num(np.array(ds), neginf = 0.0)
+        else:
+            new = np.nan_to_num(np.array(ds))
     return new
+    met = make_for_calc(f['Metallicity'])
+    stell_m = make_for_calc(f['StellarMass'], neginf = True)
+    x = make_for_calc(f['X'])
+    y = make_for_calc(f['Y'])
+    z = make_for_calc(f['Z'])
 
-met = make_for_calc(f['Metallicity'])
-stell_m = make_for_calc(f['StellarMass'], neginf = True)
-x = make_for_calc(f['X'])
-y = make_for_calc(f['Y'])
-z = make_for_calc(f['Z'])
+    new_met = []
+    new_stell_m = []
+    new_x = []
+    new_y = []
+    new_z = []
+    for i in range(len(met)):
+        if x[i] != 0 and y[i] != 0:
+            new_met.append(met[i])
+            new_stell_m.append(stell_m[i])
+            new_x.append(x[i] - gal_coord[0] - 1.8383) #correction factors, figure it out
+            new_y.append(y[i] - gal_coord[1] - 1.7024)
+            new_z.append(z[i] - gal_coord[2] - 4.2)
+    
+    list_x.append(new_x)
+    list_y.append(new_y)
+    list_z.append(new_z)
+    list_met.append(new_met)
+    list_stell_m.append(new_stell_m)
 
-new_met = []
-new_stell_m = []
-new_x = []
-new_y = []
-new_z = []
-for i in range(len(met)):
-    if x[i] != 0 and y[i] != 0:
-        new_met.append(met[i])
-        new_stell_m.append(stell_m[i])
-        new_x.append(x[i] - gal_coord[0] - 1.8383)
-        new_y.append(y[i] - gal_coord[1] - 1.7024)
-        new_z.append(z[i] - gal_coord[2] - 4.2)
-
-met = np.array(new_met)
-stell_m = np.array(new_stell_m)
-x = np.array(new_x)
-y = np.array(new_y)
-z = np.array(new_z)
+met = np.array(list_met)
+stell_m = np.array(list_stell_m)
+x = np.array(list_x)
+y = np.array(list_y)
+z = np.array(list_z)
 
 avg_met = np.sum(met * stell_m) / np.sum(stell_m)
 rad = np.sqrt(((x) ** 2) + ((y) ** 2) + ((z) ** 2))
