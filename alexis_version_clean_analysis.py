41f6a609a7388c299dc5980462ea5cd4508b0a48
amrollins <102754385+amrollins@users.noreply.github.com>
2023-02-11 14:11:04 -0600

    debugged stell m plot

--- alexis_version_clean_analysis.py
+++ alexis_version_clean_analysis.py
@@ -27,6 +27,7 @@ import math
 import sys
 import os
 import argparse
+import glob
 
 parser = argparse.ArgumentParser(description = "Inputs of CoSANG Analysis")
 parser.add_argument('--dir', action='store', dest='dir', default='', help='Path to tag files')
@@ -44,42 +45,32 @@ list_z = []
 list_stell_m = []
 
 
-for j in args.dir: #FIXME, will this loop?
-    f = h5py.File(str(j), 'r') #opens the file
+def make_for_calc(ds, neginf = False, center_val = []):
+    if neginf == True:
+        new = np.nan_to_num(np.array(ds), neginf = 0.0)
+    else:
+        new = np.nan_to_num(np.array(ds))
+    return new
+
+adress = args.dir+'/*.h5'
+
+for h5name in glob.glob(adress): #reads in all of the files in the directory
+    f = h5py.File(str(h5name), 'r') #opens the file
     f_key = list(f.keys())
     print(f_key) ##shows what keys we have to work with
-    
-    ##gets the data and puts it in the right format
-    def make_for_calc(ds, neginf = False, center_val = []):
-        if neginf == True:
-            new = np.nan_to_num(np.array(ds), neginf = 0.0)
-        else:
-            new = np.nan_to_num(np.array(ds))
-    return new
     met = make_for_calc(f['Metallicity'])
     stell_m = make_for_calc(f['StellarMass'], neginf = True)
     x = make_for_calc(f['X'])
     y = make_for_calc(f['Y'])
     z = make_for_calc(f['Z'])
 
-    new_met = []
-    new_stell_m = []
-    new_x = []
-    new_y = []
-    new_z = []
     for i in range(len(met)):
-        if x[i] != 0 and y[i] != 0:
-            new_met.append(met[i])
-            new_stell_m.append(stell_m[i])
-            new_x.append(x[i] - gal_coord[0] - 1.8383) #correction factors, figure it out
-            new_y.append(y[i] - gal_coord[1] - 1.7024)
-            new_z.append(z[i] - gal_coord[2] - 4.2)
-    
-    list_x.append(new_x)
-    list_y.append(new_y)
-    list_z.append(new_z)
-    list_met.append(new_met)
-    list_stell_m.append(new_stell_m)
+        if x[i] != 0 and y[i] != 0 and stell_m[i] > 0 and met[i]!=0:
+            list_met.append(met[i])
+            list_stell_m.append(stell_m[i])
+            list_x.append(x[i] - gal_coord[0] - 1.8383) #correction factors, figure it out
+            list_y.append(y[i] - gal_coord[1] - 1.7024)
+            list_z.append(z[i] - gal_coord[2] - 4.2)
 
 met = np.array(list_met)
 stell_m = np.array(list_stell_m)
@@ -100,21 +91,19 @@ plt.axis('auto')
 plt.title('Metallicity v. Radius')
 plt.xlabel('Radius [kpc]')
 plt.ylabel('Metallicity [Fe/H]')
-plt.show()
+plt.savefig('rad_v_met.png')
 plt.close()
 
 ##plot of metallicity weighted by solar mass
-heatmap, xedges, yedges = np.histogram2d(rad_kpc, met, weights=stell_m_for_calc,bins=50)
+heatmap, xedges, yedges = np.histogram2d(rad_kpc, met, weights=stell_m ,bins=50)
 extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 plt.clf()
-plt.imshow(np.log10(heatmap.T), extent=extent, origin='lower')
+with np.errstate(divide='ignore'):plt.imshow(np.log10(heatmap.T), extent=extent, origin='lower')
 plt.set_cmap('autumn')
 plt.colorbar(label='log10(Stellar Mass)')
 plt.title('Metallicity v. Radius, Stellar Mass weighted')
 plt.axis('auto')
 plt.xlabel('Radius [kpc]')
-plt.ylabel('Metallicity [Fe/H]')
-plt.show()
+plt.ylabel('Metallicity [metal fraction]')
+plt.savefig('stell_m_weight_rad_v_met.png')
 plt.close()
-
-f.close()
