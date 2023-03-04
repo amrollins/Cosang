1201333f760ec5549ad64f6d02d6fcb61e188fde
amrollins <102754385+amrollins@users.noreply.github.com>
2023-03-04 13:34:49 -0600

    changed limits on metallicity

--- alexis_version_clean_analysis.py
+++ alexis_version_clean_analysis.py
@@ -23,27 +23,23 @@ Created on Wed Jul  7 11:00:27 2021
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-import math 
-import sys
-import os
 import argparse
 import glob
 
 parser = argparse.ArgumentParser(description = "Inputs of CoSANG Analysis")
 parser.add_argument('--dir', action='store', dest='dir', default='', help='Path to tag files')
-parser.add_argument('--gal_cent', action='store', dest='gal_cent', default=[27.5708,29.1913,27.5166], type=list, help='coordinates of galaxy center, defaults to m12i')
+##parser.add_argument('--gal_cent', action='store', dest='gal_cent', default=[27.5708,29.1913,27.5166], type=list, help='coordinates of galaxy center, defaults to m12i')
 
 args = parser.parse_args()
 dic_args = vars(args)
 
 
-gal_coord = args.gal_cent ##maybe figure out a process to get this
+##gal_coord = args.gal_cent ##maybe figure out a process to get this
 list_met = []
 list_x = []
 list_y = []
 list_z = []
 list_stell_m = []
-
 
 def make_for_calc(ds, neginf = False, center_val = []):
     if neginf == True:
@@ -63,14 +59,26 @@ for h5name in glob.glob(adress): #reads in all of the files in the directory
     x = make_for_calc(f['X'])
     y = make_for_calc(f['Y'])
     z = make_for_calc(f['Z'])
+    ##new_x = []
+    ##new_y = []
+    ##new_z = []
 
     for i in range(len(met)):
-        if x[i] != 0 and y[i] != 0 and stell_m[i] > 0 and met[i]!=0:
+        if x[i] != 0 and y[i] != 0 and stell_m[i] > 0 and met[i]>0.000000196: ##reasonable metalliciity and stell_m limits
             list_met.append(met[i])
             list_stell_m.append(stell_m[i])
-            list_x.append(x[i] - gal_coord[0] - 1.8383) #correction factors, figure it out
-            list_y.append(y[i] - gal_coord[1] - 1.7024)
-            list_z.append(z[i] - gal_coord[2] - 4.2)
+            list_x.append(x[i] - 29.355671145527047) #correction factors, figure it out
+            list_y.append(y[i] - 31.02505460762871)
+            list_z.append(z[i] - 32.4927901257948)
+
+##gal_x = (max(new_x) + min(new_x)) / 2
+##gal_y = (max(new_y) + min(new_y)) / 2
+##gal_z = (max(new_z) + min(new_z)) / 2
+##print("Center coodinate:", gal_x, gal_y, gal_z)
+##for j in range(len(list_met)):
+  ##  list_x.append(new_x[j] - gal_x)
+  ##  list_y.append(new_y[j] - gal_y)
+  ##  list_z.append(new_z[j] - gal_z)
 
 met = np.array(list_met)
 stell_m = np.array(list_stell_m)
@@ -79,31 +87,47 @@ y = np.array(list_y)
 z = np.array(list_z)
 
 avg_met = np.sum(met * stell_m) / np.sum(stell_m)
+solar_frac_met = np.log10(met / 0.0196) ##makes metallicity in solar fraction 
 rad = np.sqrt(((x) ** 2) + ((y) ** 2) + ((z) ** 2))
+##print('x:', x)
+##print('y:', y)
+##print('z:', z)
 rad_kpc = rad * 1000
 
+plt.scatter(x,y)
+plt.savefig("x_y_pos.png")
+plt.close()
+
+plt.scatter(x,z)
+plt.savefig("x_z_pos.png")
+plt.close()
+
 ##plot of mettalicity as a function of radius
-plt.plot(rad_kpc,met,'.',markersize=1, c = 'blue')
+plt.plot(rad_kpc, met,'.',markersize=1, c = 'blue')
 m, b = np.polyfit(rad_kpc, met, 1)
 y_vals = (m * rad_kpc) + b
-plt.plot(rad_kpc, y_vals, c = 'black', linewidth = 100)
+plt.plot(rad_kpc, y_vals, c = 'black')
 plt.axis('auto')
 plt.title('Metallicity v. Radius')
 plt.xlabel('Radius [kpc]')
-plt.ylabel('Metallicity [Fe/H]')
+plt.ylabel('Metallicity, [metal fraction]')
 plt.savefig('rad_v_met.png')
 plt.close()
 
 ##plot of metallicity weighted by solar mass
-heatmap, xedges, yedges = np.histogram2d(rad_kpc, met, weights=stell_m ,bins=50)
+heatmap, xedges, yedges = np.histogram2d(rad_kpc, solar_frac_met, weights=stell_m ,bins=150)
 extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+f = plt.figure()
+f.set_figwidth(9)
+f.set_figheight(9)
 plt.clf()
 with np.errstate(divide='ignore'):plt.imshow(np.log10(heatmap.T), extent=extent, origin='lower')
-plt.set_cmap('autumn')
+plt.set_cmap('viridis')
 plt.colorbar(label='log10(Stellar Mass)')
 plt.title('Metallicity v. Radius, Stellar Mass weighted')
 plt.axis('auto')
 plt.xlabel('Radius [kpc]')
-plt.ylabel('Metallicity [metal fraction]')
+plt.ylabel('Metallicity [log10(fraction of Sun)]')
 plt.savefig('stell_m_weight_rad_v_met.png')
 plt.close()
+
